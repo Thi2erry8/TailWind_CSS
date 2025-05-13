b66e296385927d0975bc6cfa412ec340c4748e67
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-05-13 18:46:29 -0400

    color

--- react_taild/src/App.jsx
+++ react_taild/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-          <h1 class="text-3xl  font-bold underline">
+          <h1 class="text-3xl text-green-200  font-bold underline">
             Hello world!
          </h1>
     </>
